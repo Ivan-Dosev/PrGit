d759e298f0b1faf557fcef76ed25631dc142b3f5
Ivan Dimitrov <biobeehive@abv.bg>
2021-11-28 19:43:59 +0200

    Initial Commit

---
+++ PrGit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PrGit
+//
+//  Created by Dosi Dimitrov on 28.11.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PrGit/PrGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrGitApp.swift
+//  PrGit
+//
+//  Created by Dosi Dimitrov on 28.11.21.
+//
+
+import SwiftUI
+
+@main
+struct PrGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
